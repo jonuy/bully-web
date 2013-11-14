29f06629ec2e28fdcf41fa09ae702337d91ae40f
jon uy <jonxuy@gmail.com>
2013-11-14 01:21:00 -0500

    Drawing menu selector into menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -25,6 +25,7 @@ function Menu() {
   this.height = 150;
   this.entity = undefined;
   this.items = [];
+  this.selector = undefined;
   this.title = '';
 
   return this;
@@ -41,26 +42,41 @@ Menu.prototype.create = function(items) {
   var menuTopLeftX = bottomRightX - this.width - borderWidth * 2;
   var menuTopLeftY = bottomRightY - this.height - borderWidth * 2;
 
+  // Create Menu container
   this.entity = Crafty.e('Menu')
     .attr({x: menuTopLeftX, y: menuTopLeftY, w: this.width, h: this.height})
     .text('Menu Test');
 
-  var menuItemsY = menuTopLeftY + 10 /*border size*/ + 30 /* Menu title size*/;
+  // Create Menu items
+  var menuItemsX = menuTopLeftX + 10 /*border width*/ + 30 /*padding*/;
+  var menuItemsY = menuTopLeftY + 10 /*border width*/ + 30 /* Menu title size*/;
   for (i = 0; i < items.length; i++) {
     this.items[i] = Crafty.e('MenuItem')
-      .attr({x: menuTopLeftX, y: menuItemsY + i * 30, w: this.width, h: this.height})
+      .attr({x: menuItemsX, y: menuItemsY + i * 30, w: this.width, h: this.height})
       .text(items[i]);
   }
+
+  // Create Menu selector
+  var selectorX = menuItemsX - 20 /*selector size + padding*/;
+  var selectorY = menuItemsY + 5 /*for something, I don't know*/;
+  this.selector = Crafty.e('MenuSelector')
+    .attr({x: selectorX, y: selectorY, w: 0, h: 0});
 }
 
 /**
  * Menu.destroy()
  */
 Menu.prototype.destroy = function() {
+  // Remove menu container and title
   this.entity.destroy();
+
+  // Remove menu items
   for (i = 0; i < this.items.length; i++) {
     this.items[i].destroy();
   }
+
+  // Remove menu selector
+  this.selector.destroy();
 }
 
 /**
@@ -73,6 +89,7 @@ Crafty.c('MenuItem', {
         'color': 'black',
         'font-family': 'VT323',
         'font-size': '26px',
+        'text-align': 'left',
       })
       .textColor('black')
       .textFont({
@@ -80,6 +97,21 @@ Crafty.c('MenuItem', {
         family: 'VT323'
       });
   },
+});
+
+/**
+ * Menu selector component
+ */
+Crafty.c('MenuSelector', {
+  init: function() {
+    this.requires('2D, DOM, Text')
+      .css({
+        'border-style': 'solid',
+        'border-width': '10px 0 10px 17.3px',
+        'border-color': 'transparent transparent transparent #000',
+        'text-align': 'left',
+      });
+  }
 });
 
 /**
